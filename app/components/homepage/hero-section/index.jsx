632b84440a8a104dfca7447da35b5e532a02103d
Anurag Parashar Sarmah <anuragp787@gmail.com>
2024-06-10 16:02:38 +0530

    Wording Fixes

--- app/components/homepage/hero-section/index.jsx
+++ app/components/homepage/hero-section/index.jsx
@@ -89,7 +89,7 @@ function HeroSection() {
         <span className="text-amber-300">AWS</span>
         <span className="text-gray-400">{"'],"}</span>
       </div>
-      <span className="ml-4 lg:ml-8 mr-2 text-white">techWorm:</span>
+      <span className="ml-4 lg:ml-8 mr-2 text-white">techSavvy:</span>
       <span className="text-orange-400">true</span>
       <span className="text-gray-400">,</span>
       <br />
@@ -317,7 +317,7 @@ function HeroSection() {
                   {isSecondTypingComplete && (
                     <TypeIt options={optionsThird}>
                       <span className="ml-4 lg:ml-8 mr-2 text-white">
-                        techWorm:
+                        techSavvy:
                       </span>
                       <span className="text-orange-400">true</span>
                       <span className="text-gray-400">,</span>
