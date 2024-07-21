e4e74e0838ae836c09ae9a1b9e81270b28756357
Anurag Parashar Sarmah <anuragp787@gmail.com>
2024-07-21 17:40:02 +0530

    Fixes

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -12,7 +12,7 @@ function Footer() {
         </div>
         <div className="flex flex-col md:flex-row items-center justify-center">
           <p className="text-sm">
-            © Developer Portfolio by <Link target="_blank" href="https://www.linkedin.com/in/anuragpsarmah/" className="text-[#16f2b3]">Anurag Parashar Sarmah</Link>
+            Portfolio of <Link target="_blank" href="https://www.linkedin.com/in/anuragpsarmah/" className="text-[#16f2b3]">Anurag Parashar Sarmah</Link>
           </p>
         </div>
       </div>
